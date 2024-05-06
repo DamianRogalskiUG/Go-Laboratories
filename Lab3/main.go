ebf37c9b3667956c0ff474627c0fc453270e0962
Damian <d.rogalski.417@studms.ug.edu.pl>
2024-05-06 17:07:00 +0200

    Added simulation of burning all the neighbouring trees

--- Lab3/main.go
+++ Lab3/main.go
@@ -31,25 +31,52 @@ func main() {
 			}
 		}
 	}
-	fmt.Println("Las:", forest)
 
-	// random start of the fire
-	startRow, startCol := rand.Intn(rows), rand.Intn(cols)
-	forest[startRow][startCol] = burning
+	// print the original forest
+	for i := range forest {
+		fmt.Println(forest[i])
+	}
 
 	// run the simulation
-	burnForest(forest, rows, cols)
+	burntForest := burnForest(forest, rows, cols)
 
+	// print the burnt forest
+	for i := range burntForest {
+		fmt.Println(burntForest[i])
+	}
 }
 
-func burnForest(forest [][]int, rows, cols int) {
+func burnForest(forest [][]int, rows, cols int) [][]int {
+
+	// random starting point for the fire
 	startBurningX, startBurningY := rand.Intn(rows), rand.Intn(cols)
 
-	fmt.Println("Punkt startowy pożaru:", startBurningX, startBurningY)
-	fmt.Println("Las:", forest[startBurningX][startBurningY])
+	// if the starting point is a tree, set it on fire
 	if forest[startBurningX][startBurningY] == tree {
 		forest[startBurningX][startBurningY] = burning
 	}
-	fmt.Println("Las:", forest)
 
+	// queue storing coordinates of the locations to check
+	queue := [][]int{{startBurningX, startBurningY}}
+
+	// while there are trees to check, keep checking and spreading the fire
+	for len(queue) > 0 {
+		// get the coordinates of the tree to check
+		x, y := queue[0][0], queue[0][1]
+		queue = queue[1:]
+
+		// check all the neighbouring trees
+		for _, direction := range []struct{ dx, dy int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}} {
+			newX, newY := x+direction.dx, y+direction.dy
+
+			// if the tree is within the forest and is not burning, set it on fire
+			if newX >= 0 && newX < rows && newY >= 0 && newY < cols && forest[newX][newY] == tree {
+				// put the tree on fire
+				forest[newX][newY] = burning
+				// add the tree to the queue
+				queue = append(queue, []int{newX, newY})
+			}
+		}
+	}
+	return forest
 }
