283fd9a2e97b189e55e07b17a932aed198546bbf
Damian <d.rogalski.417@studms.ug.edu.pl>
2024-05-22 16:45:49 +0200

    Adding basic CRUD

--- Lab4/main.go
+++ Lab4/main.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"net/http"
 	"os"
+	"strconv"
 )
 
+// SharkAttack is a struct that represents a shark attack from the JSON file
 type SharkAttack struct {
 	Date     string `json:"date"`
 	Year     string `json:"year"`
@@ -16,9 +19,11 @@ type SharkAttack struct {
 	Location string `json:"location"`
 }
 
-var attacks []SharkAttack
+// posts is a slice of SharkAttack structs
+var posts []SharkAttack
 
-func loadSampleData() {
+// loadDataFromJSON reads the data from the JSON file and stores it in the posts slice
+func loadDataFromJSON() {
 	file, err := os.Open("global-shark-attack.json")
 	if err != nil {
 		fmt.Println("Error occured while opening the file:", err)
@@ -35,14 +40,85 @@ func loadSampleData() {
 
 	for i := 0; i < 10; i++ {
 		index := rand.Intn(len(data))
-		attacks = append(attacks, data[index])
+		posts = append(posts, data[index])
 	}
 }
 
-func main() {
-	loadSampleData()
+// getPosts returns all the posts in the posts slice
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(posts)
+}
 
-	for _, attack := range attacks {
-		fmt.Printf("%s: %s, %s, %s\n", attack.Date, attack.Type, attack.Country, attack.Area)
+// addPost adds a new post to the posts slice
+func addPost(w http.ResponseWriter, r *http.Request) {
+	var newPost SharkAttack
+	err := json.NewDecoder(r.Body).Decode(&newPost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+	posts = append(posts, newPost)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newPost)
+}
+
+// deletePost deletes a post from the posts slice
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	indexStr := r.URL.Path[len("/posts/"):]
+	index, err := strconv.Atoi(indexStr)
+	if err != nil || index < 0 || index >= len(posts) {
+		http.Error(w, "Invalid index", http.StatusBadRequest)
+		return
+	}
+
+	deletedAttack := posts[index]
+	posts = append(posts[:index], posts[index+1:]...)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(deletedAttack)
+}
+
+// updatePost updates a post in the posts slice
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	indexStr := r.URL.Path[len("/posts/"):]
+	index, err := strconv.Atoi(indexStr)
+	if err != nil || index < 0 || index >= len(posts) {
+		http.Error(w, "Invalid index", http.StatusBadRequest)
+		return
+	}
+
+	var updatedPost SharkAttack
+	err = json.NewDecoder(r.Body).Decode(&updatedPost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	posts[index] = updatedPost
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedPost)
+}
+
+// main is the entry point of the program
+func main() {
+	loadDataFromJSON()
+
+	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			getPosts(w, r)
+		} else if r.Method == http.MethodPost {
+			addPost(w, r)
+		}
+	})
+
+	http.HandleFunc("/posts/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodDelete {
+			deletePost(w, r)
+		} else if r.Method == http.MethodPut {
+			updatePost(w, r)
+		}
+	})
+
+	fmt.Println("Server is running on port 3000")
+	http.ListenAndServe(":3000", nil)
 }
